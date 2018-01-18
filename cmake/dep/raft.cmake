48128e76ecc6d4d443afa721d75b78e61df64053
Oliver Michel <oliver.michel@editum.de>
2018-01-18 13:23:03 -0700

    fix libraft cmake script

--- cmake/dep/raft.cmake
+++ cmake/dep/raft.cmake
@@ -1,6 +1,6 @@
 
-find_library(RAFT_LIB protobuf)
-find_file(RAFT_INC google/protobuf)
+find_library(RAFT_LIB raft)
+find_file(RAFT_INC raftinc)
 
 if(NOT RAFT_LIB OR NOT RAFT_INC)
     message(WARNING "raft: not found")
