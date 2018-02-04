e79e412ae0881a4bf11f766d9c701e63aaf74fa7
Oliver Michel <oliver.michel@editum.de>
2018-02-03 21:50:19 -0700

    fix cxxopts cmake dependency

--- cmake/dep/cxxopts.cmake
+++ cmake/dep/cxxopts.cmake
@@ -1,7 +1,7 @@
 
 set(CXXOPTS_VERSION 2.0.0)
 
-if(NOT EXISTS ${CMAKE_HOME_DIRECTORY}/ext/include/args/args.h)
+if(NOT EXISTS ${CMAKE_HOME_DIRECTORY}/ext/include/cxxopts/cxxopts.h)
     file(DOWNLOAD
             https://raw.githubusercontent.com/jarro2783/cxxopts/v${CXXOPTS_VERSION}/include/cxxopts.hpp
             ${CMAKE_HOME_DIRECTORY}/ext/include/cxxopts/cxxopts.h)
