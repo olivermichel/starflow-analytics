51965398ec0fdc99fbb2e725563f7ac6960afa31
Oliver Michel <oliver.michel@editum.de>
2018-02-06 16:51:28 -0700

    batching for pcap file reader

--- src/kernels/pcap_file_reader.cc
+++ src/kernels/pcap_file_reader.cc
@@ -4,21 +4,37 @@
 #include "../types/raw_packet.h"
 
 starflow::kernels::PCAPFileReader::PCAPFileReader(const std::string& file_name,
-												  outer_header_type outer_hdr)
-	: _pcap_reader(file_name, outer_hdr == outer_header_type::eth)
+												  outer_header_type outer_hdr,
+												  unsigned batch_size)
+	: _pcap_reader(file_name, outer_hdr == outer_header_type::eth),
+	  _batch_size(batch_size)
 {
-	output.add_port<std::pair<starflow::types::Key, starflow::types::Packet>>("out");
+	if (_batch_size)
+		output.add_port<_batch_out_t>("out");
+	else
+		output.add_port<_out_t>("out");
 }
 
 raft::kstatus starflow::kernels::PCAPFileReader::run()
 {
+	_batch_out_t v;
+	unsigned batch_counter = 0;
+
 	starflow::types::Key k;
 	starflow::types::Packet p;
 
 	if (!_pcap_reader.end()) {
 		_pcap_reader.next(k, p);
 		auto pair = std::make_pair(k, p);
-		output["out"].push(pair);
+
+		if (_batch_size) {
+			if (batch_counter <= _batch_size)
+				v.push_back(pair);
+			else
+				output["out"].push(v);`
+		} else {
+			output["out"].push(pair);
+		}
 		return raft::proceed;
 	} else return raft::stop;
 }
--- src/kernels/pcap_file_reader.h
+++ src/kernels/pcap_file_reader.h
@@ -12,13 +12,20 @@ namespace starflow {
 		class PCAPFileReader : public raft::kernel
 		{
 		public:
+
+			using _out_t = std::pair<starflow::types::Key, starflow::types::Packet>;
+			using _batch_out_t
+				= std::vector<std::pair<starflow::types::Key, starflow::types::Packet>>;
+
 			enum class outer_header_type { eth, ip };
 			explicit PCAPFileReader(const std::string& file_name,
-									outer_header_type outer_eth = outer_header_type::eth);
+									outer_header_type outer_eth = outer_header_type::eth,
+									unsigned batch_size = 0);
 			raft::kstatus run() override;
 
 		private:
 			modules::PCAPFileReader _pcap_reader;
+			unsigned _batch_size;
 		};
 	}
 }
