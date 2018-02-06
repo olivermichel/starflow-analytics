ce94c2067ba1b24c7e99bfdce0a3adadbeb1c90b
Oliver Michel <oliver.michel@editum.de>
2018-02-06 15:43:28 -0700

    update benchmark helper script

--- benchmark/run_all.sh
+++ benchmark/run_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-BUILD_PATH="cmake-build-release"
+BUILD_PATH=${STARFLOW_BUILD_PATH:="build"}
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 FILE.clfr"
     exit 1;
 fi
 
-$BUILD_PATH/active_connections --benchmark --input $1 > out/packet_counts.dat
+$BUILD_PATH/active_connections --benchmark --input $1 > out/active_connections.dat
 $BUILD_PATH/ewma_latencies --benchmark --input $1 > out/ewma_latencies.dat
 $BUILD_PATH/flowlet_size_histogram --benchmark --input $1 > out/flowlet_size_histogram.dat
 $BUILD_PATH/packet_counts --benchmark --input $1 > out/packet_counts.dat
